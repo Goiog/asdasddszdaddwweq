c9c9485179250f9b00561c162ef1829196469db1
Goiog <95105335+Goiog@users.noreply.github.com>
2025-10-11 13:02:38 +0200

    Create config.ts

---
+++ client/src/config.ts
@@ -0,0 +1,3 @@
+export const SUPABASE_URL_LOCAL = import.meta.env.VITE_SUPABASE_URL;
+export const SUPABASE_ANON_KEY_LOCAL = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const API_BASE_LOCAL = import.meta.env.VITE_API_BASE_URL;
